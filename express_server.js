0b34da1326a9c6a52ce42b59aac3d89a35f15b15
Kelsey Erickson <kerickson@gmx.com>
2021-08-26 22:53:11 +0000

    Add GET to redirect to longURL website

--- express_server.js
+++ express_server.js
@@ -71,6 +71,14 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get('/u/:shortURL', (req, res) => {
+
+  const longURL = urlDatabase[req.params.shortURL]
+
+  res.redirect(longURL);
+
+});
+
 
 
 

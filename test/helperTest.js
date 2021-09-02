a517ed5e14500020b5da290aa63bd041aa2c01b7
Kelsey Erickson <kerickson@gmx.com>
2021-09-02 20:21:24 +0000

    Fix linting errors

--- test/helperTest.js
+++ test/helperTest.js
@@ -4,18 +4,18 @@ const { getUserByEmail } = require('../helpers');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
-describe('getUserByEmail', function () {
+describe('getUserByEmail', function() {
 
   it('should return a user with a valid email', function() {
 

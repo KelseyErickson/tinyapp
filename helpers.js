3061917199cb15aa3a1b5c3648d9c70be7ed7e6c
Kelsey Erickson <kerickson@gmx.com>
2021-09-04 02:07:11 +0000

    Add other helper functions and export them

--- helpers.js
+++ helpers.js
@@ -12,4 +12,34 @@ const getUserByEmail = (email, database) => {
 
 };
 
-module.exports = { getUserByEmail };
+const generateRandomString = () => {
+  const availableChars = '012345789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  let randomShortURL = '';
+
+  for (let i = 0; i < 6; i++) {
+    randomShortURL += availableChars.charAt(Math.floor(Math.random() * 61));
+  }
+
+  return randomShortURL;
+
+
+};
+
+const urlsForUser = (id, database) => {
+  const userUrlDatabase = {};
+
+  for (const URL in database) {
+    const shortURL = database[URL];
+    if (id === shortURL.userID) {
+
+      userUrlDatabase[URL] = shortURL;
+    }
+    
+  
+  }
+  return userUrlDatabase;
+  
+};
+
+
+module.exports = { getUserByEmail, generateRandomString, urlsForUser };
